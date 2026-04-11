90c769faa504007d68745d15a4d50deb296d0229
aayushkash2020 <113864093+aayushkash2020@users.noreply.github.com>
2026-04-11 19:00:42 -0400

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.py
@@ -0,0 +1,35 @@
+class Solution:
+    def helper(self, nums: List[int], target: int, current: List[int],
+                answers: List[List[int]], idx: int) -> None:
+        if target == 0:
+            answers.append(current[:])
+            return
+        if idx == len(nums):
+            return
+        if nums[idx] > target:
+            return
+        self.helper(nums, target, current, answers, idx+1)
+        if nums[idx] <= target:
+            current.append(nums[idx])
+            self.helper(nums, target - nums[idx], current, answers, idx)
+            current.pop()
+
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def dfs(start, path, remaining):
+            if remaining == 0:
+                res.append(path[:])
+                return
+            for i in range(start, len(nums)):
+                if nums[i] > remaining:
+                    break
+                path.append(nums[i])
+                dfs(i, path, remaining - nums[i])
+                path.pop()
+        dfs(0, [], target)
+        return res
+        # answers = []
+        # nums.sort()
+        # self.helper(nums, target, [], answers, 0)
+        # return answers
