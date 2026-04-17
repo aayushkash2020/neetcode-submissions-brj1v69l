dedaeb2231afe927fc208742741064e28cefb373
aayushkash2020 <113864093+aayushkash2020@users.noreply.github.com>
2026-04-17 01:18:45 -0400

    Add: missing-number - submission-7

---
+++ Data Structures & Algorithms/missing-number/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return (n * (n+1)) // 2 - sum(nums)
+        # n = len(nums)
+        # found_n = False
+        # for num in nums:
+        #     if abs(num) == n:
+        #         found_n = True
+        #     else:
+        #         nums[abs(num)] *= -1
+        # if not found_n:
+        #     return n
+        # zero_idx = -1
+        # for i in range(n):
+        #     if nums[i] > 0:
+        #         return i
+        #     if nums[i] == 0:
+        #         zero_idx = i
+        # return zero_idx
