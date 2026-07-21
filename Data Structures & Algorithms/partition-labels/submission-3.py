2b8985a0caf6a7cc898ae9db773a74fc182a104b
aayushkash2020 <113864093+aayushkash2020@users.noreply.github.com>
2026-07-21 15:09:16 -0600

    Add: partition-labels - submission-3

---
+++ Data Structures & Algorithms/partition-labels/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # Kinda like merge overlapping intervals
+        first_last = {}
+        for i, ch in enumerate(s):
+            if ch in first_last:
+                first_last[ch][1] = i
+            else:
+                first_last[ch] = [i, i]
+        res = []
+        # Python dicts preserve insertion order
+        int_start = int_end = 0
+        cur_len = 0
+        for _, i in first_last.items():
+            a, b = i[0], i[1]
+            if a <= int_end:
+                int_end = max(int_end, b)
+            else:
+                res.append(cur_len)
+                int_start, int_end = a, b
+            cur_len = int_end - int_start + 1
+        res.append(cur_len)
+        return res 
