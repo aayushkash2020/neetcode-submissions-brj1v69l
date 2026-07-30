be0c7078c2428377d036014b98323706146cde1c
aayushkash2020 <113864093+aayushkash2020@users.noreply.github.com>
2026-07-29 18:18:26 -0600

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.py
@@ -0,0 +1,26 @@
+import heapq
+from collections import defaultdict
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        g = defaultdict(list)
+        weight = {}
+        for u, v, price in flights:
+            g[u].append((v, price))
+        dist = [[float('inf')] * (k+2) for _ in range(n)]
+        dist[src][0] = 0
+        to_visit = [(0, src, 0)]
+        while to_visit:
+            d, u, stops = heapq.heappop(to_visit)
+            if u == dst:
+                return d
+            if d > dist[u][stops] or stops > k:
+                continue
+            for v, price in g[u]:
+                if d + price < dist[v][stops+1]:
+                    dist[v][stops+1] = d + price
+                    heapq.heappush(to_visit, (dist[v][stops+1], v, stops + 1))
+        return -1
+
+
+
+
