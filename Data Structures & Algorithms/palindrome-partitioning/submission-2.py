bc2227a2fdf01bec882af7e7dd36fe6fa9bf9d8c
aayushkash2020 <113864093+aayushkash2020@users.noreply.github.com>
2026-08-11 18:19:29 -0600

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palindrome(l, r):
+            if l > r:
+                return False
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        n = len(s)
+        # res = final result
+        res = []
+        # path represents the current partition
+        path = []
+        def dfs(i):
+            if i == n:
+                # Append a shallow copy of the current path
+                res.append(path[:])
+                return
+            # For each of the indices i...n-1, if s[i:j+1] is a palindrome, then set end of current block to be j, then recurse
+            for j in range(i, n):
+                if is_palindrome(i, j):
+                    path.append(s[i:j+1])
+                    dfs(j+1)
+                    path.pop()
+        dfs(0)
+        return res
